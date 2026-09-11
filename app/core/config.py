9bd6567fdca9926eb582ab313ac11ea05e307aec
gl4a-dev <gl4aveloso.dev@gmail.com>
2026-09-11 13:15:06 -0300

    feat: use pydantic base settings to document/organize environment variables

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Sensor Collector API"
+
+    DB_USER: str
+    DB_PASSWORD: str 
+    DB_HOST: str
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file="./.env",
+        extra="ignore"
+    )
+
+    def get_database_url(self) -> str:
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}/{self.DB_NAME}?sslmode=require"
+    
+settings = Settings()
